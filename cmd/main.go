fde6b644806954908d113992695ad11c82733bdb
egorka <rolton301e@yandex.ru>
2025-06-06 11:34:05 +0300

    main и зависимости

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"log"
 	"todo-api/internal/config"
+	"todo-api/internal/handlers"
+	"todo-api/internal/repository/db"
 )
 
 func main() {
+
 	// Загрузка переменных окружения из .env файла
 	if err := godotenv.Load(); err != nil {
 		log.Printf("Error loading .env file: %v", err)
@@ -17,4 +21,8 @@ func main() {
 		log.Fatalf("Failed to initialize database: %v", err)
 	}
 
+	repo := db.NewPostgresRepository(config.DB)
+	app := fiber.New()
+	handlers.SetupRoutes(app, repo)
+	app.Listen(":3000")
 }
