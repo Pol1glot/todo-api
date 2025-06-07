4882b8976f5ef82a70e1a3cee7d41e2943b5827a
egorka <rolton301e@yandex.ru>
2025-06-07 11:14:52 +0300

    триггер на updated_at

--- migration/init.sql
+++ migration/init.sql
@@ -6,3 +6,21 @@ CREATE TABLE IF NOT EXISTS tasks (
     created_at TIMESTAMP DEFAULT now(),
     updated_at TIMESTAMP DEFAULT now()
     );
+
+-- Создаем функцию для обновления updated_at
+CREATE OR REPLACE FUNCTION update_timestamp_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = now();
+    IF TG_OP = 'INSERT' THEN
+        NEW.created_at = NEW.updated_at;
+END IF;
+RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+-- Создаем триггер, который будет вызывать функцию при вставке и обновлении записи
+CREATE TRIGGER update_tasks_timestamp
+    BEFORE INSERT OR UPDATE ON tasks
+                         FOR EACH ROW
+                         EXECUTE FUNCTION update_timestamp_column();
